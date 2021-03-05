83853cae0426f3bbaa159c9303c20805ec3d55e7
wuyuansushen <819070864@qq.com>
2021-03-05 19:09:57 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SequenceSum
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            double N, L;
+            string[] input = Console.ReadLine().Split(" ");
+            N = Convert.ToDouble(input[0]);
+            L = Convert.ToDouble(input[1]);
+
+            for(int n=Convert.ToInt32(L);n<=100; n++)
+            {
+                double start = (2 * N - n - n * n) / (2 * n);
+                if (start%1==0)
+                {
+                    int startReally = Convert.ToInt32(start);
+                    for(int i=startReally;i<(n+startReally-1);i++)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    Console.Write($"{n+startReally-1}");
+                    return 0;
+                }
+            }
+            Console.WriteLine("No");
+            return 0;
+        }
+    }
+}
